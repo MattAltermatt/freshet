5a9065ed30fcb8ba851b1a2d2c09f9cdd9e1f2f1
MattAltermatt <1435066+MattAltermatt@users.noreply.github.com>
2026-04-18 06:40:23 -0700

    lint: enable tsx parsing + include .tsx in lint glob

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,14 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
   env: { browser: true, node: true, es2022: true, webextensions: true },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
-  }
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
 };
